f48c1d60f46d228292500dfe45603ed6e0c37155
Siva Kumar <sivajijsa@gamil.com>
2023-03-28 20:25:09 +0530

    Version1.0

---
+++ clock.js
@@ -0,0 +1,19 @@
+let deg = 6;
+let hr = document.querySelector("#hr");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+setInterval(() => {
+    
+
+
+let day = new Date();
+let hh = day.getHours() *30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
+
+hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+min.style.transform = `rotateZ(${mm}deg)`;
+sec.style.transform = `rotateZ(${ss}deg)`;
+
+})
